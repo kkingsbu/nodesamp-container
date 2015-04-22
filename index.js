736306defc30ab4619126e9b02ed5fb209f6ff2e
Kevin Kingsbury <kkingsbu@us.ibm.com>
2015-04-22 21:41:26 +0000

    updated welcome message

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world\n');
+  res.send('Hello world. So nice to meet you.\n');
 });
 
 app.listen(PORT);
